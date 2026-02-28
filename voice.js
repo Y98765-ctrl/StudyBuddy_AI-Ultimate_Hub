70a7f9f098246aa4718b842a453dd8468cc3348a
Y98765-ctrl <y56072821@gmail.com>
2026-02-28 17:54:55 +0500

    Update voice.js

--- voice.js
+++ voice.js
@@ -1,19 +1,15 @@
 function speak(text){
-    let speech = new SpeechSynthesisUtterance(text);
-    speech.rate = 1;
-    speech.pitch = 1;
+    let speech=new SpeechSynthesisUtterance(text);
+    speech.rate=1;
     window.speechSynthesis.speak(speech);
 }
 
 function startListening(){
-    const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
-    recognition.lang = "en-US";
-
-    recognition.onresult = function(event){
-        let transcript = event.results[0][0].transcript;
-        document.getElementById("userInput").value = transcript;
-        send();
+    const recognition=new(window.SpeechRecognition||window.webkitSpeechRecognition)();
+    recognition.lang="en-US";
+    recognition.onresult=function(e){
+        document.getElementById("userInput").value=e.results[0][0].transcript;
+        sendMessage();
     }
-
     recognition.start();
 }
