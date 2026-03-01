d96c0b70ed5e3afc77580bc4cd24a67b5478f4f5
Y98765-ctrl <y56072821@gmail.com>
2026-03-01 07:43:05 +0500

    Refactor message counting and add confidence function

--- analytics.js
+++ analytics.js
@@ -1,6 +1,11 @@
-let msgCount = 0;
+let messageCount=0;
 
-function incrementMessages(){
-    msgCount++;
-    document.getElementById("msgCount").innerText = msgCount;
+function updateAnalytics(){
+    messageCount++;
+    document.getElementById("msgCount").innerText=messageCount;
+}
+
+function setConfidence(){
+    let conf=Math.floor(Math.random()*15+85);
+    document.getElementById("confidence").innerText=conf;
 }
