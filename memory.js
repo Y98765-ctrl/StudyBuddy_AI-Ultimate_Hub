c46abe17e7d464d16900882bcb1d38f6c86df0b0
Y98765-ctrl <y56072821@gmail.com>
2026-03-01 12:32:08 +0500

    Update memory.js

--- memory.js
+++ memory.js
@@ -1,10 +1,16 @@
-let MEMORY=[];
+let MEMORY = JSON.parse(localStorage.getItem("AURA_MEMORY")) || [];
 
 function memoryStore(text,type){
     MEMORY.push({text,type,time:Date.now()});
-    if(MEMORY.length>200) MEMORY.shift();
+    if(MEMORY.length>500) MEMORY.shift();
+    localStorage.setItem("AURA_MEMORY", JSON.stringify(MEMORY));
 }
 
 function getMemory(){
     return MEMORY;
+}
+
+function clearMemory(){
+    MEMORY = [];
+    localStorage.removeItem("AURA_MEMORY");
 }
